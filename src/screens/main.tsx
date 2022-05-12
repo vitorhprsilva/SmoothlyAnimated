1cf0bf90fdd869acd2d762f7c069a8442109694b
vitor <vitorhprsilva@gmail.com>
2022-05-12 11:36:11 -0300

    feat: add theme toggle to main screen

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { Text, Box, Center, VStack, themeTools, useTheme, useColorMode, useColorModeValue } from 'native-base'
+import ThemeToggle from '../components/theme-toggle'
 
 export default function MainScreen() {
     return (
         <Center 
             _dark={{bg: 'blueGray.900'}} 
-            _light={{bg: 'blueGray.500'}} 
+            _light={{bg: 'blueGray.50'}} 
             px={4} 
             flex={1}
         >
@@ -13,6 +14,7 @@ export default function MainScreen() {
                 <Box>
                     <Text>Hello</Text>
                 </Box>
+                <ThemeToggle />
             </VStack>
         </Center>
     )
