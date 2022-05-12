d0160bdcea8ab167b2c5525f0d5dcf96a6382347
vitor <vitorhprsilva@gmail.com>
2022-05-12 11:31:37 -0300

    feat: create main screen and add to app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/components/app-container';
+import MainScreen from './src/screens/main';
+
 
 export default function App() {
   return (
     <AppContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <MainScreen />
     </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
